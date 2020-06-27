60f9a3dc9b856fb5a5ccd7c3f1e5e7869a5a89c6
Bartek <bartraw23@gmail.com>
2020-06-27 19:09:50 +0200

    Initial commit

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
@@ -0,0 +1,8 @@
+package io.javabrains.moviecatalogservice.resources;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MovieCatalogResources {
+    public List<CatalogItem> getCatalog
+}
