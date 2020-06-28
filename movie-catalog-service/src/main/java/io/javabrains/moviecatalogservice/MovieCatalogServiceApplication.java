b572fa41fcff38b3ab7ecf55fb3a84957627f06a
Bartek <bartraw23@gmail.com>
2020-06-28 23:03:00 +0200

    Try to switch to webclient

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,18 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
 
-	@Bean
-	public RestTemplate getRestTemplate(){
-		return new RestTemplate();
-	}
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
+    @Bean
+    public WebClient.Builder getWebClientBuilder() {
+        return WebClient.builder();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieCatalogServiceApplication.class, args);
+    }
 
 }
 
--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -20,8 +21,12 @@ public class MovieCatalogResources {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
 
         List<Rating> ratings = Arrays.asList(
                 new Rating("1", 2),
@@ -31,6 +36,14 @@ public class MovieCatalogResources {
 
         return ratings.stream().map(rating -> {
             Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+
+           /* Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8081/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();*/
+
             return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
         }).collect(Collectors.toList());
     }
