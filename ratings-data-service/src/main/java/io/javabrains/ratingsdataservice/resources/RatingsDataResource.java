05b45d0fc518f6245df64e9306d605a7daf5cca9
Bartek <bartraw23@gmail.com>
2020-06-28 23:25:01 +0200

    Switch hardcoded list of ratings place to rating service and add new mapping

--- ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsDataResource.java
+++ ratings-data-service/src/main/java/io/javabrains/ratingsdataservice/resources/RatingsDataResource.java
@@ -5,12 +5,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsDataResource {
 
     @RequestMapping("/{movieId}")
-    public Rating getRating(@PathVariable("movieId") String movieId){
-        return new Rating(movieId,1);
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 1);
     }
+
+    @RequestMapping("/users/{userId}")
+    public List<Rating> getRatings(@PathVariable("userId") String userId) {
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1", 2),
+                new Rating("2", 4),
+                new Rating("3", 5)
+        );
+        return ratings;
+    }
+
+
 }
