1e90ba3efec87d6d97c07b6154715db975fab583
Bartek <bartraw23@gmail.com>
2020-06-30 21:43:52 +0200

    Change naming to dynamic instead of hardcoded

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resources/MovieCatalogResources.java
@@ -23,11 +23,11 @@ public class MovieCatalogResources {
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
 
-            Movie movie = restTemplate.getForObject("http://localhost:8081/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
 
             return new CatalogItem(movie.getName(), "Test desc", rating.getRating());
         }).collect(Collectors.toList());
